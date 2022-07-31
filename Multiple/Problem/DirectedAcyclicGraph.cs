ddc34177caab5451415add922cfabe63ae2008db
Vivek Jayachandran <vivek4integration@gmail.com>
2022-08-01 01:16:58 +0530

    Adding code for finding all path from two nodes in directional graph.

---
+++ Multiple/Problem/DirectedAcyclicGraph.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple.Problem
+{
+    public class DirectedAcyclicGraph
+    {
+        public static List<List<int>> AllPathsSourceTarget(int[][] graph)
+        {
+            var adjacencyList = new Dictionary<int, List<int>>();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                adjacencyList[i] = new List<int>();
+
+            }
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                for (int j = 0; graph[i] != null && j <  graph[i].Length  ; j++)
+                {
+                    if(graph[i][j]!=null)
+                    adjacencyList[i].Add(graph[i][j]);
+                }
+                
+            }
+            var routes = new List< List<int>>();
+
+            Stack<int> stack = new Stack<int>();
+            
+            stack.Push(0);
+            int routeIndex = 0;
+            routes.Add(new List<int>());
+            
+            var pathfound = false;
+            
+                while (stack.Count > 0)
+                {
+                    int node = stack.Pop();
+                    routes[routeIndex].Add(node);
+                    if (node == graph.Length - 1)
+                    {
+                        pathfound = true;
+                        routeIndex++;
+                        routes.Add(new List<int>());
+                        Stack<int> tempStack = new Stack<int>();
+                        for (int i = 0; i < stack.Count; i++)
+                        {
+                            var a = stack.Pop();
+                            tempStack.Push(a);
+                            routes[routeIndex].Add(a);
+                        }
+
+                        for (int i = 0; i < tempStack.Count ; i++)
+                        {
+                            stack.Push(tempStack.Pop());
+                        }
+                        // continue;
+                    }
+
+                  
+
+                    
+                    foreach (int i in (List<int>)adjacencyList[node])
+                    {
+                        stack.Push(i);
+                    }
+               
+
+                //if (pathfound)
+                //{
+                //    //routeIndex++;
+                //    //routes.Add(new List<int>());
+                //    //stack.Clear();
+                //    //stack.Push(0);
+                    
+                //    pathfound = false;
+                //}
+                //else
+                //{
+                //    break;
+                //}
+            }
+
+            return routes;
+        }
+    }
+}
---
+++ Multiple/Problem/DirectedAcyclic_OthersSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple.Problem
+{
+    public class DirectedAcyclic_OthersSolution
+    {
+        public static IList<IList<int>> allPathsSourceTarget(int[][] graph)
+        {
+            List<IList<int>> paths = new List<IList<int>>();
+            if (graph == null || graph.Length == 0)
+            {
+                return paths;
+            }
+
+            dfs(graph, 0, new List<int>(), paths);
+            return paths;
+        }
+
+        private static void dfs(int[][] graph, int node, List<int> path, List<IList<int>> paths)
+        {
+            path.Add(node);
+            if (node == graph.Length - 1)
+            {
+                paths.Add(new List<int>(path));
+                return;
+            }
+            int[] nextNodes = graph[node];
+            foreach (var nextNode in nextNodes)
+            {
+               dfs(graph, nextNode, path,paths);
+               path.RemoveAt(path.Count-1);
+            }
+        }
+    }
+}
